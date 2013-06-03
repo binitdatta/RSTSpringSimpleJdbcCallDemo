3f09aa6fc4f02fd82fcb053dc9a3f1621ded9d8c
Binit Datta <binitdatta@gmail.com>
2013-06-03 16:22:02 -0500

    changed from interface to class

--- src/main/java/com/rollingstone/spring/simplejdbc/dao/IRSTConstrants.java
+++ src/main/java/com/rollingstone/spring/simplejdbc/dao/IRSTConstrants.java
@@ -9,18 +9,18 @@ public class IRSTConstrants {
 	/*
 	 * Function names for Oracle Stored functions
 	 */
-	static String F_EMP_GET_APPRAISAL_REMARKS = "F_EMP_GET_APPRAISAL_REMARKS";
+	public static String F_EMP_GET_APPRAISAL_REMARKS = "F_EMP_GET_APPRAISAL_REMARKS";
 
-	static String F_EMP_CREATE_APPRAISAL_REMARKS = "F_EMP_CREATE_APPRAISAL_REMARKS";
+	public static String F_EMP_CREATE_APPRAISAL_REMARKS = "F_EMP_CREATE_APPRAISAL_REMARKS";
 
-	static String F_EMP_CREATE_EMPLOYEE = "F_EMP_CREATE_EMPLOYEE";
+	public static String F_EMP_CREATE_EMPLOYEE = "F_EMP_CREATE_EMPLOYEE";
 
-	static String F_EMP_DELETE_EMPLOYEE = "F_EMP_DELETE_EMPLOYEE";
+	public static String F_EMP_DELETE_EMPLOYEE = "F_EMP_DELETE_EMPLOYEE";
 
-	static String F_EMP_UPDATE_EMPLOYEE = "F_EMP_UPDATE_EMPLOYEE";
+	public static String F_EMP_UPDATE_EMPLOYEE = "F_EMP_UPDATE_EMPLOYEE";
 
-	static final String DATE_OF_JOIN = "01" + "/" + "01" + "/" + "1985";
+	public static final String DATE_OF_JOIN = "01" + "/" + "01" + "/" + "1985";
 
-	static final String DATE_OF_BIRTH = "01" + "/" + "01" + "/" + "1960";
+	public static final String DATE_OF_BIRTH = "01" + "/" + "01" + "/" + "1960";
 
 }
